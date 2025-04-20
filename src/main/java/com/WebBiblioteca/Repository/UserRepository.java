72d9524f68ffb9f25c30c27f53856fad2afb2cba
Michael <micgaelma@gmail.com>
2025-04-19 22:41:31 -0500

    Meetodos implementados en la clase repositorio de usuario

--- src/main/java/com/WebBiblioteca/Repository/UserRepository.java
+++ src/main/java/com/WebBiblioteca/Repository/UserRepository.java
@@ -3,14 +3,92 @@ package com.WebBiblioteca.Repository;
 import com.WebBiblioteca.Model.User;
 import org.springframework.stereotype.Repository;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class UserRepository{
-    private List<User> usersList = new LinkedList<>();
+    private final AtomicLong idGenerator = new AtomicLong(1);
+    private final HashMap<Long,User> usersMap = new HashMap<>();
 
-    public List<User> getUsersList(){
-        return usersList;
+    //listar usuarios
+    public HashMap<Long,User> getUsersList(){
+        return usersMap;
     }
+
+    //listar usuarios por su estado
+    public Map<Long,User> getUsersByState(Boolean state){
+        return usersMap.entrySet().stream()
+                .filter(entry -> entry.getValue().getState().equals(state))
+                .collect(HashMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), HashMap::putAll);
+    }
+
+    //buscar usuario por su id
+    public User getUserById(Long id){
+        return usersMap.get(id);
+    }
+
+    //buscar usuario por su email
+    public User getUserByEmail(String email){
+       return usersMap.values().stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
+    }
+
+    //agregar usuario
+    public User addUser(User user){
+        Long id = idGenerator.getAndIncrement();
+        user.setCode(id);
+        user.setDateRegistered(LocalDateTime.now());
+        user.setState(true);
+        usersMap.put(id, user);
+        return user;
+    }
+
+    //loguear usuario
+    public User loginUser(String email, String password){
+        return usersMap.values().stream()
+                .filter(user -> user.getEmail().equals(email) && user.getPassword().equals(password) && user.getState())
+                .findFirst()
+                .orElse(null);
+    }
+
+    // actualizar usuario
+    public User updateUser(Long id, User user){
+        return usersMap.computeIfPresent(id, (key, existingUser) -> {
+            existingUser.setName(user.getName());
+            existingUser.setLastname(user.getLastname());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setPhone(user.getPhone());
+            existingUser.setAddress(user.getAddress());
+            existingUser.setDNI(user.getDNI());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setState(user.getState());
+            return existingUser;
+        });
+
+    }
+
+    // eliminar usuario
+    public void deleteUser(Long id){
+        usersMap.remove(id);
+    }
+    // acutualizar algunos campos
+    public User partialUpdate(Long id, User user){
+        return usersMap.computeIfPresent(id, (key, existingUser) -> {
+            if (user.getName() != null) existingUser.setName(user.getName());
+            if (user.getLastname() != null) existingUser.setLastname(user.getLastname());
+            if (user.getEmail() != null) existingUser.setEmail(user.getEmail());
+            if (user.getPhone() != null) existingUser.setPhone(user.getPhone());
+            if (user.getAddress() != null) existingUser.setAddress(user.getAddress());
+            if (user.getDNI() != null) existingUser.setDNI(user.getDNI());
+            if (user.getPassword() != null) existingUser.setPassword(user.getPassword());
+            if (user.getState() != null) existingUser.setState(user.getState());
+            return existingUser;
+        });
+    }
+
 }
